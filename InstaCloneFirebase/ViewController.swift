0dc0d150326f184d268d8e09c7812ccaeaaa575d
Oguzhan Bekir <oguzhanbekir53@gmail.com>
2020-08-25 13:24:37 +0300

    Initial Commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Oguzhan Bekir on 25.08.2020.
+//  Copyright © 2020 Oguzhan Bekir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
