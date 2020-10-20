754cf5529aa9925dbeaf01f1c1d36077a9193937
Oguzhan Bekir <oguzhanbekir53@gmail.com>
2020-10-20 22:43:03 +0300

    first commit

--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
                 } else {
                     self.performSegue(withIdentifier: "toFeedVC", sender: nil)
                 }
+                
             }
         } else {
             makeAlert(titleInput: "Error", messageInput: "Please fill email and password")
